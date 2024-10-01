14fa6ca698e8d237f22b360a900192e6247a39cb
Mihail Komiser <komiser@gmail.com>
2024-10-01 21:36:19 +0300

    добавлены комментарии

--- selenium_python.py
+++ selenium_python.py
@@ -1,10 +1,12 @@
+# Импортируем необходимые библиотеки и модули
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+options = webdriver.ChromeOptions()  # Создаем объект ChromeOptions для настройки браузера
+options.add_experimental_option("detach", True)  # Включаем режим отсоединения окна браузера от консоли
+# Устанавливаем соединение с браузером Chrome с использованием ChromeDriver
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://www.saucedemo.com/'
-driver.get(base_url)
-driver.maximize_window()
+base_url = 'https://www.saucedemo.com/'  # Устанавливаем базовый URL для доступа к сайту
+driver.get(base_url) # Переходим на указанный URL
+driver.maximize_window() # Максимизируем окно браузера
