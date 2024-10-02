19d9ddea6385d0c69d4476eab1dc18b925fff5f4
Mihail Komiser <komiser@gmail.com>
2024-10-02 17:49:27 +0300

    авторизация на сайте

---
+++ Authorization on the site.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+# Находим элемент поля Username  с идентификатором "user-name"
+user_name = driver.find_element(By.ID, "user-name")
+# помещаем текст "standard_user" в найденный элемент
+user_name.send_keys('standard_user')
+# Находим элемент поля Password  с идентификатором "password"
+password = driver.find_element(By.ID, 'password')
+# помещаем текст "secret_sauce" в найденный элемент
+password.send_keys("secret_sauce")
+# Находим локатор кнокпи Login
+button_login = driver.find_element(By.ID, 'login-button')
+# Вызываем метод Click - нажатие на кнопку
+button_login.click()
