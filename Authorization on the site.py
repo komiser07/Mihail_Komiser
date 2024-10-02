58bf6bd21d59b806e47ff81701a397db21cdd0a5
Mihail Komiser <komiser@gmail.com>
2024-10-02 18:49:15 +0300

    авторизация на сайте

--- Authorization on the site.py
+++ Authorization on the site.py
@@ -26,22 +26,3 @@ button_login = driver.find_element(By.ID, 'login-button')
 button_login.click()
 print("Click Login Button")
 print(driver.current_url)
-
-# Проверка на корректность URL
-get_url = driver.current_url
-# Вывод фактического URL
-url = 'https://www.saucedemo.com/inventory.html'
-# Сравнение ожидаемого результата и фактического
-assert url == get_url
-# вывод сообщение что проверка удачна
-print("URL корректен")
-
-# Проверка что заголовок на странице каталога совпадает с ожидаемым результатом
-text_products = driver.find_element(By.XPATH, "//span[@class='title']")
-# Вывод текста заголовка по локатору
-print(text_products.text)
-value_text_products = text_products.text
-# Проверка что заголовок соответствует слову "Products"
-assert value_text_products == 'Products'
-# вывод сообщение что проверка удачна
-print("Заголовок корректен")
