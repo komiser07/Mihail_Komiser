845263bf08454ff2b1e08f23a57857b58d92b956
Mihail Komiser <komiser@gmail.com>
2024-10-07 19:49:03 +0300

    загрузка файла

--- Uploading a file in the browser.py
+++ Uploading a file in the browser.py
@@ -14,16 +14,25 @@ driver.maximize_window()
 # прописываем в переменную путь до файла
 path_upload = "D:\\Users\\komis\\PycharmProjects\\Selenium_python\\files_upload\\screenshot2024.10.03-16.27.02.png"
 
-# находим локатор для кнопки "Выберите файл" и использум метод "send_keys"
+# находим локатор для кнопки "Выберите файл"
 click_button = driver.find_element(By.XPATH, "//*[@id='file']")
 
 # используем метод "send_keys" для загрузки файла
 click_button.send_keys(path_upload)
 print("Файл успешно загружен")
+# Получаем имя загруженного файла
+uploaded_file_name = driver.find_element(By.XPATH, "//*[@id='file']").get_attribute('value')
+print(uploaded_file_name)
+removing_the_parts = uploaded_file_name.split('\\')
+file_name = removing_the_parts[-1]
+print(file_name)
 
+# Ожидаемое имя файла
+expected_file_name = "screenshot2024.10.03-16.27.02.png"
+print(expected_file_name)
 
-
-
+assert file_name == expected_file_name
+print("файлы совпадают")
 
 
 
