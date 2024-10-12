d29a922906520d9cef9aaff8caa369e371d8cc01
Mihail Komiser <komiser@gmail.com>
2024-10-12 18:33:39 +0300

    класс и метод

--- OOP + Selenium/Creating a class and method.py
+++ OOP + Selenium/Creating a class and method.py
@@ -1,28 +1,32 @@
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-options = webdriver.EdgeOptions()
-options.add_experimental_option("detach", True)
+from edge_selenium import options
+
 
 # создаём класс для тестирования
-class Test():
-# создаём метод для авторизации
-    def test_authoization(self):
+class Test:
+    # создаём метод для инициализации драйвера и открытия сайта
+    def __init__(self):
+        self.driver = webdriver.Edge(options=options, service=EdgeService(EdgeChromiumDriverManager().install()))
+        self.base_url = 'https://www.saucedemo.com/'
+        self.driver.get(self.base_url)
+        self.driver.maximize_window()
 
-        driver = webdriver.Edge(options=options, service=EdgeService(EdgeChromiumDriverManager().install()))
-        base_url = 'https://www.saucedemo.com/'
-        driver.get(base_url)
-        driver.maximize_window()
-
-        driver.find_element(By.ID, "user-name").send_keys('standard_user')
+        self.driver.find_element(By.ID, "user-name").send_keys('standard_user')
         print("Input Login")
-        driver.find_element(By.ID, 'password').send_keys("secret_sauce")
+        self.driver.find_element(By.ID, 'password').send_keys("secret_sauce")
         print("Input Password")
-        driver.find_element(By.ID, 'login-button').click()
+        self.driver.find_element(By.ID, 'login-button').click()
         print("Click Login Button")
+
+    # создаём метод для закрытия окна браузера
+    def quit(self):
+        self.driver.quit()
+
 
 # создаём экземпляр класса и запускаем тест
 start_test = Test()
-start_test.test_authoization()
+start_test.quit()
