76052af3894cc11cccc8e545c3516f735d458701
Mihail Komiser <komiser@gmail.com>
2024-10-12 22:54:40 +0300

    класс и метод

--- OOP + Selenium/Creating a class and method.py
+++ OOP + Selenium/Creating a class and method.py
@@ -2,29 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
 from edge_selenium import options
 
 
 # создаём класс для тестирования
 class Test:
     # создаём метод для инициализации драйвера и открытия сайта
-    def __init__(self, username, password):
+    def _initialize_browser(self):
         self.driver = webdriver.Edge(options=options, service=EdgeService(EdgeChromiumDriverManager().install()))
-        self.base_url = 'https://www.saucedemo.com/'
-        self.driver.get(self.base_url)
+        base_url = 'https://www.saucedemo.com/'
+        self.driver.get(base_url)
         self.driver.maximize_window()
 
+    # создаём метод для закрытия окна браузера
+    def quit(self):
+        self.driver.quit()
+        print("Close Browser")
+
+    # создаём метод для авторизации на сайте
+    def __init__(self, username, password):
+        self._initialize_browser()
+        self._authorize(username, password)
+
+    def _authorize(self, username, password):
         self.driver.find_element(By.ID, "user-name").send_keys(username)
         print("Input User Name")
         self.driver.find_element(By.ID, 'password').send_keys(password)
         print("Input Password")
         self.driver.find_element(By.ID, 'login-button').click()
         print("Click Login Button")
-
-    # создаём метод для закрытия окна браузера
-    def quit(self):
-        self.driver.quit()
 
 
 # создаём экземпляр класса и запускаем тест
