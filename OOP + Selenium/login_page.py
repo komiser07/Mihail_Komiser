1804c2dd41483ad4795c71d1a48a27dde296f7bc
Mihail Komiser <komiser@gmail.com>
2024-10-13 23:43:14 +0300

    Создание модуля Авторизации

--- OOP + Selenium/login_page.py
+++ OOP + Selenium/login_page.py
@@ -3,15 +3,13 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-
-
-
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
     def authorization(self, login_name, login_password):
-        WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='user-name']"))).send_keys(login_name)
+        WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='user-name']"))).send_keys(
+            login_name)
         print("Input User Name")
         WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, 'password'))).send_keys(login_password)
         print("Input Password")
